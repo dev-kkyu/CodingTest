d1706ff742b26e949fe470453e268c9440aa9ecf
SEONG KYU LIM <104413535+dev-kkyu@users.noreply.github.com>
2024-08-16 05:21:07 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -4,12 +4,11 @@
 using namespace std;
 
 long long solution(int n) {
-    vector<int> fibo{1, 2, 3, 5, 8};
-    fibo.resize(n + 1);
-    for(int i=2;i<n + 1; ++i){
-        fibo[i] = fibo[i - 1] + fibo[i - 2];
-        fibo[i] %= 1234567;
+    vector<int> dp{0, 1, 2, 3, 5, 8};
+    dp.resize(n + 1);
+    for(int i = 3; i < n + 1; ++i){
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
     }
     
-    return fibo[n - 1];
+    return dp[n];
 }
