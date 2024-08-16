adf0cdd72030a9496c56537c21c8fe2479becdc8
SEONG KYU LIM <104413535+dev-kkyu@users.noreply.github.com>
2024-08-16 22:06:06 +0900

    [level 2] Title: 괄호 회전하기, Time: 19.80 ms, Memory: 4.26 MB -BaekjoonHub

---
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <list>
+#include <stack>
+#include <unordered_map>
+
+using namespace std;
+
+bool isValidString(const list<char>& strs)
+{
+    unordered_map<char, char> charMap;
+    charMap['['] = ']';
+    charMap['{'] = '}';
+    charMap['('] = ')';
+    
+    stack<char> stackStr;
+    
+    for(char c : strs){
+        if (stackStr.empty()){
+            stackStr.push(c);
+        }
+        else{          
+            if (c == charMap[stackStr.top()])
+                stackStr.pop();
+            else
+                stackStr.push(c);
+        }
+    }
+    return stackStr.empty();
+}
+
+int solution(string s) {
+    list<char> strs{s.begin(), s.end()};
+    
+    int answer = 0;
+    for(int i = 0;i< s.size();++i){
+        if (isValidString(strs))
+            ++answer;
+        strs.push_back(strs.front());
+        strs.pop_front();
+    }
+    
+    return answer;
+}
