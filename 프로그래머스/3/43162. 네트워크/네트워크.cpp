1d940a9fc7ece8721881d6cd7783a9d697855ff9
SEONG KYU LIM <104413535+dev-kkyu@users.noreply.github.com>
2024-08-17 04:33:23 +0900

    [level 3] Title: 네트워크, Time: 0.07 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+using namespace std;
+
+void my_search(set<int>& sets, int index, int n, const vector<vector<int>>& computers)
+{
+    sets.erase(index);
+    for(int i = 0; i < n; ++i){
+        if (i != index and 1 == computers[index][i]){
+            if (sets.find(i) != sets.end()){
+                my_search(sets, i, n, computers);
+            }
+        }
+    }
+}
+
+int solution(int n, vector<vector<int>> computers) {
+    set<int> nums;
+    for(int i=0;i<n;++i)
+        nums.insert(i);
+    
+    int answer = 0;
+    while(nums.size() > 0){
+        my_search(nums, *nums.begin(), n, computers);
+        ++answer;
+    }
+    return answer;
+}
