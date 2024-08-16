fc57cb8efb5ebade0a43c2da705d09998bb87e6d
SEONG KYU LIM <104413535+dev-kkyu@users.noreply.github.com>
2024-08-16 21:48:41 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1210.19 ms, Memory: 20.3 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -1,13 +1,12 @@
-#include <string>
 #include <vector>
-#include <set>
+#include <unordered_set>
 
 using namespace std;
 
 int solution(vector<int> elements) {
     int size = elements.size();
     
-    set<int> sets;
+    unordered_set<int> sets;
     
     for(int i = 1; i<= size;++i){       // 부분집합 1 ~ 5개 일때
         for(int j = 0; j < size; ++j){  // 각 부분집합별로 경우의 수 (항상 size)
@@ -15,7 +14,7 @@ int solution(vector<int> elements) {
             for(int k = 0;k < i; ++k){  // 부분집합 개수에 따라서 합 구하기
                 sum += elements[(k + j) % size];
             }
-            sets.insert(sum);
+            sets.emplace(sum);
         }
     }
     
