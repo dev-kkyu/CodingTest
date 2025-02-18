f1d5afdb5f53191e0651451b8f4e9e551d5b2894
SEONG KYU LIM <104413535+dev-kkyu@users.noreply.github.com>
2025-02-19 01:35:35 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    
+    int complete = 0;
+    
+    for(int day = 1; ; ++day){
+        if (progresses[complete] + speeds[complete] * day >= 100){
+            // 배포 준비 완료
+            int count = 1;
+            while(true){
+                if (complete + count >= progresses.size())
+                    break;
+                if (progresses[complete + count] + speeds[complete + count] * day < 100)
+                    break;
+                ++count;
+            }
+            answer.push_back(count);
+            complete += count;
+        }
+        
+        if (complete >= progresses.size())
+            break;
+    }
+    
+    return answer;
+}
