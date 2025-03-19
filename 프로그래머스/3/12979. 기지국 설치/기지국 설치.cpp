1b9baa516131246c96de1dcbee3ebb52b37177cd
SEONG KYU LIM <104413535+dev-kkyu@users.noreply.github.com>
2025-03-20 05:46:42 +0900

    [level 3] Title: 기지국 설치, Time: 0.09 ms, Memory: 3.9 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.cpp
@@ -6,34 +6,24 @@ using namespace std;
 
 int solution(int n, vector<int> stations, int w)
 {
-    list<pair<int, int>> remainAparts;    // 전파가 안닿는 아파트의 시작~끝 리스트
-
+    int answer = 0;
+    
+    int effect = w * 2 + 1; // 영향을 끼치는 범위
     int startPos = 1;
     
     for(int s : stations){
-        int left = s - w;   // 영향을 끼치는 범위
+        int left = s - w;   // 영향을 끼치는 위치
         int right = s + w;
         
-        if (left < 1)
-            left = 1;
-        if (right > n)
-            right = n;
-        
         if (startPos < left){
-            remainAparts.push_back({startPos, left - 1});
+            int dist = left - startPos; // 영향이 닿지 않는 거리
+            answer += (dist + effect - 1) / effect;     // dist/effect 올림 연산
         }
         startPos = right + 1;
     }
     
     if (startPos <= n){
-        remainAparts.push_back({startPos, n});
-    }
-    
-    int answer = 0;
-    
-    for (const auto& remain : remainAparts){
-        int dist = remain.second - remain.first + 1;
-        int effect = w * 2 + 1;
+        int dist = n - startPos + 1; // 영향이 닿지 않는 거리
         answer += (dist + effect - 1) / effect;
     }
     
